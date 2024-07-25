6a843910139d8bbda433253574305892e1eb9f03
dpclfk <lee@iseungbaeui-MacBookAir.local>
2024-07-25 14:47:31 +0900

    fix:detail address

--- front/src/Component/Modal/Buy/UserAdressItem.tsx
+++ front/src/Component/Modal/Buy/UserAdressItem.tsx
@@ -7,18 +7,13 @@ interface IProps {
   id: number;
 }
 
-const AdressItem = ({
-  detail,
-  item,
-  selectadress,
-  id,
-}: IProps): JSX.Element => {
+const AdressItem = ({ detail, item, selectadress, id }: IProps): JSX.Element => {
   return (
     <div className="p-4 flex gap-5 text-[1.2rem] ">
       <input
         className="h-[1.5rem] w-[1.5rem]"
         type="radio"
-        value={item}
+        value={item + " " + detail}
         name="buy"
         onChange={(e: ChangeEvent<HTMLInputElement>) => {
           selectadress(e.target.value, id);
